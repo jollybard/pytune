36edb8002d69d474a41bbb5370f70fb401c8d7c2
jollybard <thejollybard@gmail.com>
2020-04-30 21:53:34 -0400

    MOS generator

--- mos.py
+++ mos.py
@@ -5,6 +5,9 @@ def mod(x,p):
     else:
         return x % p
 
+def edo(n,m):
+    return n * (1200/m)
+
 def generateScale(x,n,m,p):
     scale = [i*x for i in range(-m,-m+n)]
     scale = [mod(x,p) for x in scale]
@@ -16,20 +19,32 @@ def fareyNeighbor(a,b):
         k = a*i // b + 1
         if b*k - a*i == 1:
             return (k,i)
+    return (0,1)
             
 def mosGenRanges(n,p):
     l = [[(i, n),fareyNeighbor(i,n)] for i in range(1,n)]
     return [[i * p / n, ii * p/ nn] for [(i,n),(ii,nn)] in l]
 
-def generatorHasMOS(g,n):
-    ranges = mosGenRanges(n)
+def generatorHasMOS(g,n,p):
+    ranges = mosGenRanges(n,p)
     for [a,b] in ranges:
         if a <= g <= b:
+            return True
+        if a <= (p - g) <= b:
             return True
     return False
 
 def intervalTable(l):
     n = len(l)
-    l = [[mod(l[(i+j) % n] - l[j], l[n]) for i in range(n)] for j in range(n)]
+    l = [[mod(l[(i+j) % n] - l[j], l[-1]) for i in range(n)] for j in range(n)]
     return l
 
+# generate the MOS scales of a given (generator,period) pair
+# TODO: make a scale object
+
+def mosSizes(g,p):
+    n = 1
+    while True:
+        if generatorHasMOS(g,n,p):
+            yield n
+        n += 1
